b84a73ee1aa24e87accdef3b76fecfc5b9cf116a
Khaidir Mauladan <kdr.mauladan@gmail.com>
2023-04-16 16:15:45 +0700

    rework

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# khadrmd.github.io
+# project-submission-khadrmd
+project-submission-khadrmd created by GitHub Classroom
+
+This project is developed to fulfill RevoU's Software Engineering Mini Course
+
+Deployment: [khadrmd.github.io](https://khadrmd.github.io)
--- scripts.js
+++ scripts.js
@@ -3,8 +3,24 @@ function namePrompt() {
   if (name == null) {
     name = "User";
   }
-  let username = document.getElementById("username");
-  username.innerText = name;
+  document.getElementById("username").innerText = name;
+  document.getElementById("form-name").value = name;
 }
 
-function submitForm() {}
+function submitForm(event) {
+  event.preventDefault();
+  var name = document.forms["form"]["form-name"].value;
+  var birthdate = document.forms["form"]["form-birthdate"].value;
+  var gender = document.forms["form"]["gender"].value;
+  var pesan = document.forms["form"]["form-pesan"].value;
+
+  if (pesan == "") pesan = "-";
+
+  document.getElementById("res-time").innerText = Date();
+  document.getElementById("res-name").innerText = name;
+  document.getElementById("res-birthdate").innerText = birthdate;
+  document.getElementById("res-gender").innerText = gender;
+  document.getElementById("res-pesan").innerText = pesan;
+}
+
+function imageSlide(n) {}

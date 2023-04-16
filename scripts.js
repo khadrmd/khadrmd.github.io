e0b41b43ada6d1f9a282fee611a4031f4d2191d9
Khaidir Mauladan <kdr.mauladan@gmail.com>
2023-04-16 18:01:50 +0700

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+function __init__() {
+  namePrompt();
+  initSlidingImg();
+}
+
 function namePrompt() {
   let name = prompt("What is your name?", "User");
   if (name == null) {
@@ -7,8 +12,7 @@ function namePrompt() {
   document.getElementById("form-name").value = name;
 }
 
-function submitForm(event) {
-  event.preventDefault();
+function submitForm() {
   var name = document.forms["form"]["form-name"].value;
   var birthdate = document.forms["form"]["form-birthdate"].value;
   var gender = document.forms["form"]["gender"].value;
@@ -21,6 +25,31 @@ function submitForm(event) {
   document.getElementById("res-birthdate").innerText = birthdate;
   document.getElementById("res-gender").innerText = gender;
   document.getElementById("res-pesan").innerText = pesan;
+
+  return false;
 }
 
-function imageSlide(n) {}
+function initSlidingImg() {
+  images = document.getElementsByClassName("banner-img");
+  for (i = 1; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+}
+
+slideIndex = 0;
+var images;
+
+function nextImg(n) {
+  if (slideIndex + n > images.length - 1) slideIndex = 0;
+  else if (slideIndex + n < 0) slideIndex = images.length - 1;
+  else slideIndex += n;
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+
+  images[slideIndex].style.display = "block";
+}
+
+setInterval(function () {
+  nextImg(1);
+}, 5000);
